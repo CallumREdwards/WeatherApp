bd725d166bcf8a24af50a3564969030f214db311
fbogd <fbogdan9919@gmail.com>
2019-05-15 11:49:53 +0100

    Added ImageView

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,41 +2,29 @@
 //
 //import javafx.application.Application;
 //import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
 //import javafx.scene.Scene;
-//import javafx.scene.control.Label;
-//import javafx.scene.layout.VBox;
+//import javafx.scene.layout.Pane;
 //import javafx.stage.Stage;
-//import javafx.scene.control.Button;
 //
 //public class Main extends Application{
 //    private Stage window;
-//    private Scene MainScreen, SettingsScreen;
-//
-//    @Override
-//    public void start(Stage primaryScene) throws Exception{
-//        Parent MScreen = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
-//        Parent SScreen = FXMLLoader.load(getClass().getResource("SettingsScreen.fxml"));
-//
-//        MainScreen = new Scene (MScreen, 1080, 1920);
-//        SettingsScreen = new Scene (SScreen, 1080, 1920);
-//
-//        window = primaryScene;
-//
-//        window.setScene(MainScreen);
-//        window.setTitle("Main Screen");
-//        window.show();
-//    }
-//
-//
-//
+//    private Pane MScreen;
+//    private Pane SScreen;
 //
 //
 //    public static void main(String[] args) {
 //        launch(args);
 //    }
 //
+//    @Override
+//    public void start(Stage primaryScene) throws Exception{
+//        MScreen = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+//        SScreen = FXMLLoader.load(getClass().getResource("SettingsScreen.fxml"));
 //
+//        window = primaryScene;
 //
-//
+//        window.setScene(new Scene(MScreen, 1080, 1920));
+//        window.setTitle("Main Screen");
+//        window.show();
+//    }
 //}
--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -1,13 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.event.Event;
-
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -17,66 +16,85 @@ public class MainController extends Application{
     }
 
     private Stage window;
-    private Pane MScreen = new Pane();
-    private Pane SScreen = new Pane();
+    private Pane MScreen;
+    private Pane SScreen;
 
-    private TextField location = new TextField();
-    private Button settings = new Button();
-    private TextField lastUpdated = new TextField();
-    private TextField safe = new TextField();
-    private ImageView imageForSafe = new ImageView();
+    @FXML
+    Button Settings;
 
-    public void initializeButtons(){
-        location.setEditable(false);
-        location.setLayoutX(15.0);
-        location.setLayoutY(14.0);
-        location.setPrefHeight(110.951171875);
-        location.setPrefWidth(454.0);
-        location.setText("Location");
+    @FXML
+    Button SafeButton;
 
-        settings.setLayoutX(469.0);
-        settings.setLayoutY(56.0);
-        settings.setMnemonicParsing(false);
-        settings.setPrefWidth(71.0);
-        settings.setText("Settings");
+    @FXML
+    Button TemperatureButton;
 
-        lastUpdated.setEditable(false);
-        lastUpdated.setLayoutX(15.0);
-        lastUpdated.setLayoutY(125.0);
-        lastUpdated.setPrefWidth(454.0);
-        lastUpdated.setText("lastUpdated");
+    @FXML
+    Button UVButton;
 
-        safe.setEditable(false);
-        safe.setLayoutX(106.0);
-        safe.setLayoutY(181.0);
-        safe.setPrefHeight(68.0);
-        safe.setPrefWidth(363.0);
-        safe.setText("Safe");
+    @FXML
+    Button WindButton;
 
-        imageForSafe.setFitHeight(68.0);
-        imageForSafe.setFitWidth(90.66666666666666);
-        imageForSafe.setLayoutX(15.0);
-        imageForSafe.setLayoutY(181.0);
-        imageForSafe.pickOnBoundsProperty();
-        imageForSafe.preserveRatioProperty();
+    @FXML
+    Button TideButton;
 
-        MScreen.getChildren().add(location);
-        MScreen.getChildren().add(settings);
-        MScreen.getChildren().add(lastUpdated);
-        MScreen.getChildren().add(safe);
-        MScreen.getChildren().add(imageForSafe);
-    }
 
     @Override
     public void start(Stage primaryScene) throws Exception{
-        initializeButtons();
+        MScreen = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        SScreen = FXMLLoader.load(getClass().getResource("SettingsScreen.fxml"));
 
         window = primaryScene;
 
-        window.setScene(new Scene (MScreen, 1080, 1920));
+        window.setScene(new Scene(MScreen, 1080, 1920));
         window.setTitle("Main Screen");
         window.show();
     }
 
-
 }
+
+//    public void initializeButtons(){
+//        location.setEditable(false);
+//        location.setLayoutX(15.0);
+//        location.setLayoutY(14.0);
+//        location.setPrefHeight(110.951171875);
+//        location.setPrefWidth(454.0);
+//        location.setText("Location");
+//
+//        settings.setLayoutX(469.0);
+//        settings.setLayoutY(56.0);
+//        settings.setMnemonicParsing(false);
+//        settings.setPrefWidth(71.0);
+//        settings.setText("Settings");
+//
+//        lastUpdated.setEditable(false);
+//        lastUpdated.setLayoutX(15.0);
+//        lastUpdated.setLayoutY(125.0);
+//        lastUpdated.setPrefWidth(454.0);
+//        lastUpdated.setText("lastUpdated");
+//
+//        safe.setEditable(false);
+//        safe.setLayoutX(106.0);
+//        safe.setLayoutY(181.0);
+//        safe.setPrefHeight(68.0);
+//        safe.setPrefWidth(363.0);
+//        safe.setText("Safe");
+//
+//        imageForSafe.setFitHeight(68.0);
+//        imageForSafe.setFitWidth(90.66666666666666);
+//        imageForSafe.setLayoutX(15.0);
+//        imageForSafe.setLayoutY(181.0);
+//        imageForSafe.pickOnBoundsProperty();
+//        imageForSafe.preserveRatioProperty();
+//
+//        MScreen.getChildren().add(location);
+//        MScreen.getChildren().add(settings);
+//        MScreen.getChildren().add(lastUpdated);
+//        MScreen.getChildren().add(safe);
+//        MScreen.getChildren().add(imageForSafe);
+//    }
+
+//    private TextField location = new TextField();
+//    private Button settings = new Button();
+//    private TextField lastUpdated = new TextField();
+//    private TextField safe = new TextField();
+//    private ImageView imageForSafe = new ImageView();
