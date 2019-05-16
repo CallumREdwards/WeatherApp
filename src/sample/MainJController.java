a7f6c4c0bc65c4cf92dd4033492694a8555b51b4
JennyLynnFletcher <jenny.lynn.fletcher@gmail.com>
2019-05-16 14:08:33 +0100

    design layout added

---
+++ src/sample/MainJController.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.util.Date;
+
+public class MainJController {
+
+    private String location_text;
+    private Boolean warning_icon_visible;
+    private String weather_icon_type;
+    private Double temperature_value;
+    private String uv_level_text;
+    private Double wind_direction_degrees;
+    private String current_tide_text;
+    private String next_tide_text;
+    private String current_tide_time_text;
+    private String next_tide_time_text;
+    private String last_update_text;
+    private String date_text;
+    private String time_text;
+    private Double wind_speed_value;
+    private Boolean wind_speed_metric;
+    private String safety_rating;
+
+    public void update_values() {
+        
+    }
+
+
+}
